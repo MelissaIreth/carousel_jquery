6dbcc409d9376470d6d799f0f6f29a97c0a7d0e5
Melissa <melissa.pacheco.martinez@gmail.com>
2017-07-07 20:55:42 -0400

    show img

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,35 @@
-$(function() {
+    var imgindex = 0,
+      items = $('.container div'),
+      i = items.length;
 
-    var images = ['240', '260', '250', '123', '200'];
-    var max = images.length;
- 
-    // Get current image src
-    var curSrc = $('#imageswap').attr('src');
-
-    // Find ID in image src
-    var curID = curSrc.replace(/.*\/(.*?)\.jpg/i, '$1');
-
-    var curIdx = 0;
-
-    // Search image list for index of current ID
-    while (curIdx < max) {
-        if (images[curIdx] == curID) {
-            break;
-        }
-        curIdx++;
+    function ciclo() {
+      var item = $('.container div').eq(imgindex);
+      items.hide();
+      item.css('display','inline-block');
     }
 
-    // For convenience
-    var imgSrcBase = 'http://placehold.it/';
-    
-    // Next image on button (and image) click
-    $('#swapnextimg,#imageswap').click( function() {
-        curIdx = (curIdx+1) % max;
-        $("#imageswap").attr('src', imgSrcBase+images[curIdx]+'.jpg');
-    });
-    
-    // Prev image on button click
-    $('#swapprevsimg').click( function() {
-        curIdx = (curIdx+max-1) % max;
-        $("#imageswap").attr('src', imgSrcBase+images[curIdx]+'.jpg');
+    var autoSlide = setInterval(function() {
+      imgindex += 1;
+      if (imgindex > i - 1) {
+        imgindex = 0;
+      }
+      ciclo();
+    }, 5000);
+
+    $('.next').click(function() {
+      clearInterval(autoSlide);
+      imgindex += 1;
+      if (imgindex > i - 1) {
+        imgindex = 0;
+      }
+      ciclo();
     });
 
-
-});
+    $('.prev').click(function() {
+      clearInterval(autoSlide);
+      imgindex -= 1;
+      if (imgindex < 0) {
+        imgindex = i - 1;
+      }
+      ciclo();
+    });
